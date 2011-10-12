c75f3c61d15ef97d93557926c6528c8d788ad29e
GodOfROFL <valentin-dietrich@gmx.de>
2011-10-12 23:36:46 +0300

    scho besser

--- main.cpp
+++ main.cpp
@@ -13,9 +13,42 @@ int main(int argc, char **argv)
     return 1;
   }
 
-  cout << "Willkommen zum SDL Testprogramm." << endl;
+  // Fenstertitel setzen:
+  // Der erste Text steht in der Titelleiste des Fensters und der zweite in der Taskleiste.
+  SDL_WM_SetCaption( "Tomprogs Game Tutorials - First Try", "Tomprogs - First Try" );
 
+  // Doublebuffering aktivieren
+  SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER, 1 );
 
+  // Ein OpenGL Fenster mit einer Auflösung von 800*600 Pixeln und
+  // einer Farbtiefe von 32 Pixeln erzeugen.
+  SDL_SetVideoMode( 800, 600, 32, SDL_OPENGL );
+  
+  while(true)
+  {
+    SDL_Event event; // Hier wird immer die neue Nachricht gespeichert
+  
+    while( SDL_PollEvent(&event) ) // Holen der nächsten Nachricht
+    {
+      // Bestimmen des Nachrichtentyps
+      switch(event.type)
+      {
+        // Bei einer SDL_QUIT Nachricht sollte das Programm beendet werden.
+        // Sie wird zb. verschickt wenn man auf das X beim Programmfenster
+        // klickt.
+        case SDL_QUIT:
+          SDL_Quit(); // Herunterfahren der SDL
+          exit(0);
+          break;
+        default:
+          break;
+      }
+    }
+
+  // Hierher kommt dann alles was das Spiel am Laufen hält...
+
+  }
+  
   SDL_Quit(); // Herunterfahren der SDL Bibliotheken
   return 0;
 }
